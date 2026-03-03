74c1ce115906935533494a88b554197c1862ce38
stylusseoul <stylus.seoul@gmail.com>
2026-03-03 20:39:57 +0900

    Update config.js

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ const COVER_PLACEHOLDER = "images/logo.png";
 
 // 🔥 신청곡 기능
 const REQUESTS_GVIZ_URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:json&sheet=Requests`;
-const REQUEST_API_URL = "https://script.google.com/macros/s/AKfycbxHjaVyocV8PdRCDLjzcsyFXV5mNScNRZlbSoiNny8fvmOWzMv-_V_G-tHL3wIKh1asPA/exec";
+const REQUEST_API_URL = "https://script.google.com/macros/s/AKfycbw41YjZsaH8uVZL_xVeHy5Y58cfwSgVSiTSyuPPkVzcO3wBhsCYXakhpOwzEP55_U3XQA/exec";
