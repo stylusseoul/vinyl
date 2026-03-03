68e978d76070f261401bf51073b508b2a0fdacfe
stylusseoul <stylus.seoul@gmail.com>
2026-03-03 21:44:24 +0900

    Fix REQUEST_API_URL endpoint typo

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ const COVER_PLACEHOLDER = "images/logo.png";
 
 // 🔥 신청곡 기능
 const REQUESTS_GVIZ_URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:json&sheet=Requests`;
-const REQUEST_API_URL = "https://script.google.com/macros/s/AKfycbwui78wgo3_TGWrCxdRkCrS0d8-HyDgUNTVvp2yNHWG9ZOhdllbcdDYJfY5FFjZ0rTJgw/exec";
+const REQUEST_API_URL = "https://script.google.com/macros/s/AKfycbwui78wgo3_TGWrCxdRkCrS0d8-HyDgUNTVvp2yNHWG9ZOhdllbcdDYJfY5FFjZ0rTJgw/execexec";
